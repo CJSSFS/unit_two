46605adaffd02e64ecd3e98e3de23bf40c77a508
CJ <cj@iMac7.local>
2018-09-19 08:57:24 -0400

    Got user name location and started number

--- assignment_two.py
+++ assignment_two.py
@@ -1,1 +1,8 @@
-User_1 = "Hello Whats your name?"
+user = input("Hello, Whats your name?")
+print("Hello",user)
+print("Its nice to meet you. My name is Dan")
+location = input("Where are you from?")
+print("Sounds like a great place to be from!")
+number = input("What is your favorite number?")
+
+
