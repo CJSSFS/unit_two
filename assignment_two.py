8543bee201ea2ba292c3c369e93e5e3c1091f3ac
CJ <cj@iMac7.local>
2018-09-20 10:18:20 -0400

    found rate, cost, and monthly payment of car

--- assignment_two.py
+++ assignment_two.py
@@ -3,6 +3,17 @@ print("Hello",user)
 print("Its nice to meet you. My name is Dan")
 location = input("Where are you from?")
 print("Sounds like a great place to be from!")
-number = input("What is your favorite number?")
+number = float(input("What is your favorite number?"))
+print("my favorite number is 4 .")
+print("Your number plus my number is", number + 4)
+car = input("What is your dream car?")
+print("Wow! ive always wanted a", car)
+cost = float(input("What is the price of the car?"))
+print("That is pretty expensive!")
+loan = float(input("How many years would it take to pay out a loan for the car?"))
+rate = float(input("What is the annual interest rate expected for the car?"))
+rate = rate / 100 / 12
+loan = loan
+monthly_pay = (rate * cost) / (1 - (1 + rate) ** - loan * 12)
 
 
