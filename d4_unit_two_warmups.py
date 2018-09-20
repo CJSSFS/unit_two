0feb5708b0a9a8e6914cec573c0229662ff660e6
CJ <cj@iMac7.local>
2018-09-20 09:15:44 -0400

    Mathematical Operations

---
+++ d4_unit_two_warmups.py
@@ -0,0 +1,13 @@
+print(9 * 5)
+print(2 / 5)
+print(12 // 5)
+print(27 // 4)
+print(2 // 5)
+print(5 % 2)
+print(9 % 5)
+print(6 % 6)
+print(2 % 7)
+# the // is how many times the second number goes into the first number
+# The percent is the remainder after dividing
+
+
