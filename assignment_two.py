812e31155a3a79317b778d5da1b36652b2b7f5c5
CJ <cj@iMac7.local>
2018-09-21 11:13:26 -0400

    Found out the interest rate and the total of the car and said goodbye

--- assignment_two.py
+++ assignment_two.py
@@ -1,3 +1,7 @@
+# Chad Scott
+# 09/21/18
+# This is an assignment where the Chatbot is able to speak with the user to find out information about buying a car.
+
 user = input("Hello, Whats your name?")
 print("Hello",user)
 print("Its nice to meet you. My name is Dan")
@@ -13,7 +17,11 @@ print("That is pretty expensive!")
 loan = float(input("How many years would it take to pay out a loan for the car?"))
 rate = float(input("What is the annual interest rate expected for the car?"))
 rate = rate / 100 / 12
-loan = loan
-monthly_pay = (rate * cost) / (1 - (1 + rate) ** - loan * 12)
+monthly_pay = (rate * cost) / (1 - (1 + rate) ** (-loan * 12))
+print(monthly_pay)
+print(monthly_pay * loan)
+print("If you bought a ", car, "you would have a monthly payment of", monthly_pay, "I hope it fits your budget")
+print("The total would be", monthly_pay * loan, "I hope you can purchase it. Anyways i have to go. Goodbye.")
+
 
 
